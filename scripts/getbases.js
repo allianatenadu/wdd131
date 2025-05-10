6e33a512eb6f86dcb2bfa181c2a826e5cd165bee
alliana-tenadu <allianaotenadu@gmail.com>
2025-05-10 06:13:07 +0000

    done

---
+++ scripts/getbases.js
@@ -0,0 +1,5 @@
+// Get the current year for the copyright
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get the last modified date
+document.getElementById('lastModified').textContent = `Last modification: ${document.lastModified}`;
